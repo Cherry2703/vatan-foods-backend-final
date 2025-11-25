97611642f78db568ec884738f511460139404382
Cherry2703 <ramcharanamr2408@gmail.com>
2025-11-25 22:09:24 +0530

    changes in uri in db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
+console.log("mongo uri", process.env.MONGO_URI);
+
+const MONGO_URI = "mongodb+srv://ramcharanamr2408_db_user:kZ3UaaVlEZzoBUH8@cluster0.kcqwxxv.mongodb.net/vatanFoodsDB"
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(MONGO_URI, {
       autoIndex: true,
     });
 
