66a5c32f0bc1b1f0871992f0ceb82765027ae1f9
Cherry2703 <ramcharanamr2408@gmail.com>
2025-11-26 06:30:49 +0530

    changes in cleaning routes

--- routes/cleaningRoutes.js
+++ routes/cleaningRoutes.js
@@ -1,20 +1,20 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import Cleaning from "../models/Cleaning.js";
-import Incoming from "../models/Incoming.js";
 import { protect } from "../middleware/authMiddleware.js";
 import History from "../models/History.js";
 
-const app = express.Router();
+const router = express.Router();
 
-//--------------------------------------------------
 async function saveHistory(batchId, model, action, data, updatedBy) {
   await History.create({ batchId, model, action, data, updatedBy });
 }
 
-// Cleaning ------------------------------------------------------------
-app.post("/", protect, async (req, res) => {
+// Create
+router.post("/", protect, async (req, res) => {
   try {
+    req.body.cleaningId = uuidv4();
+    req.body.createdBy = req.user.name || req.user.id;
     const clean = await Cleaning.create(req.body);
     await saveHistory(clean.batchId, "Cleaning", "CREATE", clean, req.user.name);
     res.json(clean);
@@ -23,27 +23,27 @@ app.post("/", protect, async (req, res) => {
   }
 });
 
-
-app.get("/", protect, async (req, res) => {
+// Get all
+router.get("/", protect, async (req, res) => {
   const data = await Cleaning.find({});
   res.json(data);
 });
 
-app.get("/:cleaningId",protect, async (req, res) => {
+// Get single
+router.get("/:cleaningId", protect, async (req, res) => {
   const data = await Cleaning.findOne({ cleaningId: req.params.cleaningId });
   res.json(data);
 });
 
-app.put("/:id", protect, async (req, res) => {
+// Update
+router.put("/:cleaningId", protect, async (req, res) => {
   try {
-    const previous = await Cleaning.findOne({cleaningId:req.params.id});
-    //const updated = await Cleaning.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const previous = await Cleaning.findOne({ cleaningId: req.params.cleaningId });
     const updated = await Cleaning.findOneAndUpdate(
-      { cleaningId: req.params.id },
+      { cleaningId: req.params.cleaningId },
       req.body,
       { new: true }
     );
-
     await saveHistory(updated.batchId, "Cleaning", "UPDATE", { previous, updated }, req.user.name);
     res.json(updated);
   } catch (err) {
@@ -51,8 +51,8 @@ app.put("/:id", protect, async (req, res) => {
   }
 });
 
-
-app.delete("/:cleaningId", protect, async (req, res) => {
+// Delete
+router.delete("/:cleaningId", protect, async (req, res) => {
   try {
     const deleted = await Cleaning.findOneAndDelete({ cleaningId: req.params.cleaningId });
     await saveHistory(req.params.cleaningId, "Cleaning", "DELETE", deleted, req.user.name);
@@ -62,6 +62,4 @@ app.delete("/:cleaningId", protect, async (req, res) => {
   }
 });
 
-
-
-export default app;
+export default router;
