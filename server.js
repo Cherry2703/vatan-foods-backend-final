e12c09a30af60e91648ddb464dc841f78fc3bb54
Cherry2703 <ramcharanamr2408@gmail.com>
2025-11-27 14:48:24 +0530

    added client url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
+    origin: [process.env.CLIENT_URL, "http://localhost:3000","https://vatan-foods.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
